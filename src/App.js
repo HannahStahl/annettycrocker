dbbd652527350e4dc95d294edd04fb5282b4c25c
Hannah Stahl <hls62@cornell.edu>
2018-12-06 10:39:57 -0600

    Added alt to img

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <span>Coming soon! </span>
-          <span className="App-emoji-span"><img className="App-emoji" src="./yummy-emoji.png" /></span>
+          <span className="App-emoji-span"><img className="App-emoji" src="./yummy-emoji.png" alt="emoji" /></span>
         </header>
       </div>
     );
