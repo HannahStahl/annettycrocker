00b67eab022df72cdc37f5056c86567e5c16e539
Hannah Stahl <hls62@cornell.edu>
2018-12-07 16:59:49 -0600

    Added instafeed photos and changed Coming Soon to order request button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import Instafeed from 'instafeed.js';
 import './App.css';
 
 class App extends Component {
   render() {
+    var feed = new Instafeed({
+      get: 'user',
+      userId: '9436320028',
+      accessToken: '9436320028.d6b2746.de3414163bb3419dac64d022bd252833',
+      template: '<img src="{{image}}" class="App-photo" />'
+    });
+    feed.run();
     return (
       <div className="App">
-        <header className="App-header">
-          <span>Coming soon! </span>
-          <span className="App-emoji-span"><img className="App-emoji" src="./yummy-emoji.png" alt="emoji" /></span>
-        </header>
+        <div id="instafeed" />
+        <Button className="App-button">Request custom order!</Button>
       </div>
     );
   }
