86d269485c8ea4fc3efc5ceb1d34768010bf755c
Hannah Stahl <hls62@cornell.edu>
2018-12-13 00:01:11 -0600

    Implemented email send

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ var feed = new Instafeed({
 });
 feed.run();
 
+const sendEmailURL = "https://snzk8dcwt2.execute-api.us-east-1.amazonaws.com/dev/email/send";
+
 class App extends Component {
   constructor(props, context) {
     super(props, context);
@@ -28,7 +30,9 @@ class App extends Component {
       message: '',
       showErrorOnName: false,
       showErrorOnEmail: false,
-      showErrorOnMessage: false
+      showErrorOnMessage: false,
+      emailAttemptComplete: false,
+      emailAttemptResponse: ''
     };
   }
 
@@ -44,7 +48,7 @@ class App extends Component {
       message: '',
       showErrorOnName: false,
       showErrorOnEmail: false,
-      showErrorOnMessage: false      
+      showErrorOnMessage: false     
     });
   }
 
@@ -59,18 +63,42 @@ class App extends Component {
     }
   }
 
-  handleSubmit() {
-    this.setState({
+  async handleSubmit() {
+    await this.setState({
       showErrorOnName: this.state.name.length === 0,
       showErrorOnEmail: validator.isEmail(this.state.email) === false,
       showErrorOnMessage: this.state.message.length === 0
     });
-    if (!(
+    const allFieldsValid = !(
       this.state.showErrorOnName ||
       this.state.showErrorOnEmail ||
       this.state.showErrorOnMessage
-    )) {
-      // TODO - send email from backend
+    );
+    if (allFieldsValid) {
+      var req = new XMLHttpRequest();
+      req.open("POST", sendEmailURL, true);
+      req.setRequestHeader("Content-Type", "application/json");
+      req.addEventListener("load", function () {
+        var emailAttemptResponse = 'Sent!';
+        if (req.status >= 400) {
+          emailAttemptResponse = 'Oops! Something went wrong. Please try emailing me at annettycrocker@gmail.com.';
+        }
+        this.setState({
+          name: '',
+          email: '',
+          message: '',
+          showErrorOnName: false,
+          showErrorOnEmail: false,
+          showErrorOnMessage: false,
+          emailAttemptComplete: true,
+          emailAttemptResponse
+        });
+      }.bind(this));
+      req.send(JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+        content: this.state.message
+      }));
     }
   }
 
@@ -80,51 +108,58 @@ class App extends Component {
         <div id="instafeed" />
         <Modal className="App-modal" show={this.state.show} onHide={this.handleClose}>
           <Modal.Header className="App-modal-header" closeButton />
-          <Modal.Body>
-            <p>I would love to bake for you! Please include the details of what you are looking for below. I will get back to you shortly to discuss!</p>
-            <Form horizontal className="App-form">
-              <FormGroup validationState={this.state.showErrorOnName ? 'error' : null}>
-                <Col componentClass={ControlLabel} sm={2}>Name:</Col>
-                <Col sm={10}>
-                  <FormControl
-                    className="App-form-field App-short-field"
-                    type="text"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                  />
-                </Col>
-              </FormGroup>
-              <FormGroup validationState={this.state.showErrorOnEmail ? 'error' : null}>
-                <Col componentClass={ControlLabel} sm={2}>Email:</Col>
-                <Col sm={10}>
-                  <FormControl
-                    className="App-form-field App-short-field"
-                    type="email"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                  />
+          {!this.state.emailAttemptComplete && <div>
+            <Modal.Body>
+              <p>I would love to bake for you! Please include the details of what you are looking for below. I will get back to you shortly to discuss!</p>
+              <Form horizontal className="App-form">
+                <FormGroup validationState={this.state.showErrorOnName ? 'error' : null}>
+                  <Col componentClass={ControlLabel} sm={2}>Name:</Col>
+                  <Col sm={10}>
+                    <FormControl
+                      className="App-form-field App-short-field"
+                      type="text"
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.handleChange}
+                    />
                   </Col>
-              </FormGroup>
-              <FormGroup validationState={this.state.showErrorOnMessage ? 'error' : null}>
-                <Col componentClass={ControlLabel} sm={2}>Message:</Col>
-                <Col sm={10}>
-                  <FormControl
-                    className="App-form-field App-message"
-                    componentClass="textarea"
-                    name="message"
-                    value={this.state.message}
-                    onChange={this.handleChange}
-                  />
-                </Col>
-              </FormGroup>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer className='App-modal-footer'>
-            <Button className="App-button App-cancel-button" onClick={this.handleClose}>Cancel</Button>
-            <Button className="App-button App-send-button" type="submit" bsStyle="primary" onClick={this.handleSubmit}>Send</Button>
-          </Modal.Footer>
+                </FormGroup>
+                <FormGroup validationState={this.state.showErrorOnEmail ? 'error' : null}>
+                  <Col componentClass={ControlLabel} sm={2}>Email:</Col>
+                  <Col sm={10}>
+                    <FormControl
+                      className="App-form-field App-short-field"
+                      type="email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                    />
+                    </Col>
+                </FormGroup>
+                <FormGroup validationState={this.state.showErrorOnMessage ? 'error' : null}>
+                  <Col componentClass={ControlLabel} sm={2}>Message:</Col>
+                  <Col sm={10}>
+                    <FormControl
+                      className="App-form-field App-message"
+                      componentClass="textarea"
+                      name="message"
+                      value={this.state.message}
+                      onChange={this.handleChange}
+                    />
+                  </Col>
+                </FormGroup>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer className='App-modal-footer'>
+              <Button className="App-button App-cancel-button" onClick={this.handleClose}>Cancel</Button>
+              <Button className="App-button App-send-button" type="submit" bsStyle="primary" onClick={this.handleSubmit}>Send</Button>
+            </Modal.Footer>
+          </div>}
+          {this.state.emailAttemptComplete && <div>
+            <Modal.Body>
+              <p>{this.state.emailAttemptResponse}</p>
+            </Modal.Body>
+          </div>}
         </Modal>
         <div className="App-order-button-container">
           <Button className="App-button App-order-button" onClick={this.handleShow}>Request custom order!</Button>
